10108ddc1d0dff99bce8ef6056aa9c025db5b572
leerivera <leerivera80@gmail.com>
2022-03-22 19:53:01 -0400

    first4~

--- class21-materials/mighty-morphin-review/js/main.js
+++ class21-materials/mighty-morphin-review/js/main.js
@@ -5,9 +5,7 @@ favHoliday = "xmas"
 console.log(favHoliday.toUpperCase())
 //Declare a variable, assign it a string, alert the last three characters in the string (Use your google-fu and the MDN)
 let word = "bigHunk"
-alert(word[2])
-alert(word[3])
-alert(word[4])
+console.log(word.slice(-3))
 // *Functions*
 // Create a function that takes in 5 numbers. Subtract all five from 100. Alert the absolute value of the difference. Call the function.
 function subNums(n1, n2, n3, n4, n5){
@@ -16,7 +14,13 @@ function subNums(n1, n2, n3, n4, n5){
 }
 subNums(75, 45, 89, 68, 87)
 // Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
+function highAndLow(n1,n2,n3){
+    let min = Math.min(n1,n2,n3)
+    let max = Math.max(n1,n2,n3)
+    console.log(`low${min} high${max}`)
 
+}
+highAndLow(39,34,76)
 
 // *Conditionals*
 //Create a function that returns heads or tails randomly and as fairly as possible. Call the function.
