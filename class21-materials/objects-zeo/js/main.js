28b9025daae73212f4dc1d16a266e67e468cd835
leerivera <leerivera80@gmail.com>
2022-03-22 21:34:12 -0400

    first9~

--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,19 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {}
+
+stopwatch.color = "black"
+stopwatch.brand ="cisco"
+stopwatch.shape = "round"
+stopwatch.size = "small"
+
+stopwatch.start = function(){
+    console.log("start")
+}
+
+stopwatch.stop = function(){
+    console.log("stop")
+}
+
+stopwatch.sayBrand = function(){
+    console.log(stopwatch.brand)
+}
