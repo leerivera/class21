fdbb52c7c929dc2ffcd8fbd051f131f2d1b1c770
leerivera <leerivera80@gmail.com>
2022-03-22 20:56:39 -0400

    first7~

--- class21-materials/alien-rangers-review/js/main.js
+++ class21-materials/alien-rangers-review/js/main.js
@@ -1,9 +1,14 @@
 //Arrays
 
 //Create and array of tv shows. Loop through and print each show to the console
-
+let shows = ["cheers", "dogs", "brats", "bitches"]
+shows.forEach(show => console.log(show))
 //Create and array of numbers
-//Return a new array of numbers that includes every even number from the previous Arrays
+let nums =[21, 78, 98,100]
 
+//Return a new array of numbers that includes every even number from the previous Arrays
+let onlyEven = arr => arr.filter(n => n % 2 === 0)
+
+console.log(onlyEven(nums))
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
